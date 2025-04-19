2240fa60a5e00b43905fcd284ef9cf0dfb1fba1a
Oshi-code1234 <ushaniprarthana164@gmail.com>
2025-04-19 10:43:10 +0530

    Add files via upload

---
+++ tickets/Events.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tickets
+{
+    internal class Events
+    {
+        private string name;
+        private string date;
+        private string location;
+        private string des;
+        //private Tiket eventtik;
+
+        public Events(string name, string date, string location, string des)
+        {
+            this.name = name;
+            this.date = date;
+            this.location = location;
+            this.des = des;
+            //Tiket eventtik , this.eventtik = eventtik;
+        }
+
+        public string Nameget
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Dateget
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        public string Locget
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        public string Desget
+        {
+            get { return des; }
+            set { des = value; }
+        }
+
+        //public Tiket EventtikGet
+        //{
+        //    get { return eventtik; }
+        //    set { eventtik = value; }
+        //}
+
+        public void ViewEvents()
+        {
+            Console.WriteLine($"Event is {Nameget}.Date is {Dateget}.Location is {Locget}.It is a {Desget}.");
+        }
+        public void EditEvent()
+        {
+
+            Console.WriteLine("1.Name, 2.Date, 3.Location,4.Description,5.Non ");
+            Console.WriteLine("What do you want to change|Enter the number: ");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            if (answer == 1)
+            {
+                Console.WriteLine("Enter the new name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Name is changed to {Nameget = name}");
+            }else if (answer == 2)
+            {
+                Console.WriteLine("Enter the new date: ");
+                string date = Console.ReadLine();
+                Console.WriteLine($"Name is changed to {Dateget = date}");
+            }
+            else if (answer == 3)
+            {
+                Console.WriteLine("Enter the new location: ");
+                string loc = Console.ReadLine();
+                Console.WriteLine($"Name is changed to {Locget = loc}");
+            }
+            else if (answer == 4)
+            {
+                Console.WriteLine("Enter the new date: ");
+                string des = Console.ReadLine();
+                Console.WriteLine($"Name is changed to {Dateget = des}");
+            }
+            else if (answer == 5)
+            {
+                return;
+            }
+
+        }
+
+    }
+}
---
+++ tickets/Program.cs
@@ -0,0 +1,89 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Net.Sockets;
+using System.Xml.Linq;
+using tickets;
+
+//Console.WriteLine("Hello, World!");
+//*Events CRUD * (Create, Edit, Delete, View)
+//- *Event Details * (Name, Date, Location, Description)
+//- *Admin Dashboard * (Track ticket sales & revenue)  
+
+//Event arrey and driver arrey
+Events[] eventArrey = new Events[1];
+Tiket[] tiketArrey = new Tiket[1];
+
+Events e1 = new Events("MyEvent", "1.2.2028", "Ghampha", "Musical Event");
+Tiket t1 = new Tiket("VIP", 8000);
+
+
+
+List<Events> eventlist = new List<Events>();
+
+
+
+e1.ViewEvents();
+Console.WriteLine();
+
+//event1.EditEvent();
+//Console.WriteLine();
+
+//Console.WriteLine("New Things are: ");
+//event1.ViewEvents();
+
+//create new event
+//Events[] eventArrey = new Events[1];
+//for (int i = 0; i < eventArrey.Length; i++)
+//{
+//    Console.WriteLine("Event");
+//    Console.WriteLine("Enter the Event name: ");
+//    string ename = Console.ReadLine();
+//    Console.WriteLine();
+
+//    Console.WriteLine("Enter the Date: ");
+//    string edate = Console.ReadLine();
+//    Console.WriteLine();
+
+//    Console.WriteLine("Enter the location: ");
+//    string eloc = Console.ReadLine();
+//    Console.WriteLine();
+
+//    Console.WriteLine("Enter the Des: ");
+//    string edes = Console.ReadLine();
+//    Console.WriteLine();
+
+//    Events newevent = new Events(ename, edate, eloc, edes);
+//    eventArrey[i] = newevent;
+//    i.ViewEvents();
+//}
+
+//foreach (Events newevent in eventArrey)
+//{
+//    newevent.ViewEvents();
+//    Console.WriteLine();
+
+//    newevent.EditEvent();
+//    Console.WriteLine();
+
+//    Console.WriteLine("New Things are: ");
+//    newevent.ViewEvents();
+//}
+
+//ticket fee
+Tiket ticket1 = new Tiket("Mytiket", 1000);
+Console.WriteLine("How many tickets you want a: ");
+int tik = Convert.ToInt32(Console.ReadLine());
+
+//ticket1.TickSlaes(ticket1.TikName, tik);
+
+
+//int[] array = { 1, 2, 3, 4, 5 };
+//array = array.Where(val => val != 4).ToArray();
+
+//string[] array1 = {"me" , "miine" , "mera" };
+//eventArrey = eventArrey.Where(val => val != "mine").ToArray();
+
+//foreach (string va in eventArrey)
+//{
+//    Console.WriteLine($"{va}");
+//}
